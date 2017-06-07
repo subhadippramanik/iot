a33c5ad6a1d38ebef6256a34f2eb503d75138fe0
subhadippramanik <pramaniksubhadip@gmail.com>
2017-06-07 23:51:16 +0530

    feat: publisher -> app

---
+++ publisher.js
@@ -0,0 +1,26 @@
+var PubNub = require('pubnub')
+
+var pubnub = new PubNub({
+    subscribeKey: "sub-c-6a36b378-4adc-11e7-bf50-02ee2ddab7fe",
+    publishKey: "pub-c-61959443-f381-4e17-aab2-f7c1470c1033",
+    secretKey: "sec-c-NmQ5MWUzNmYtMmJiMC00YTUxLWJkNDItOThhMTJlZWQ2NTY1",
+    ssl: true
+})
+
+exports.publish= function(jsonMessage) {
+    pubnub.publish({
+        message: jsonMessage,
+        channel: 'chnl-desired',
+        sendByPost: false, // true to send via post
+        storeInHistory: false, //override default storage options
+        meta: {
+            "appId": "1"
+        } // publish extra meta with the request
+    },
+    function (status, response) {
+        // handle status, response
+        console.log(status)
+        console.log(response)
+    }
+    );
+}
